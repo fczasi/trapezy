ef9e05da47a118e00a54136da921ec51d47e495c
fczasi <jetcarb893@gmail.com>
2021-11-30 15:02:25 +0100

    tak

---
+++ app/src/main/java/com/example/trapezy/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.trapezy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button obl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        obl = findViewById(R.id.oblicz);
+
+
+    }
+    public void scd(View view)
+    {
+        Toast.makeText(MainActivity.this,"tak".Toast.LENGHT_SHORT).show();
+    }
+    public void sp(View view)
+    {
+
+    }
+    public boolean email() {
+        matches.
+    }
+}
---
+++ app/src/main/java/com/example/trapezy/obltrap.java
@@ -0,0 +1,14 @@
+package com.example.trapezy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class obltrap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obltrap);
+    }
+}
